92e7f6be28590cf03bc44ac2a3cd97d35ef166fb
Varsha Singh <varsha.singh0497@gmail.com>
2021-12-09 21:50:23 +0530

    feat: delete transaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,6 +1,5 @@
 import React, { createContext, useReducer } from "react";
-import AppReducer from './AppReducer';
-
+import AppReducer from "./AppReducer";
 
 // initial state
 const initialState = {
@@ -17,11 +16,24 @@ export const GlobalContext = createContext(initialState);
 
 // provider component
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
-    
-    return(<GlobalContext.Provider value={{
-        transactions: state.transactions
-    }}>
-        {children}
-    </GlobalContext.Provider>)
-}
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
