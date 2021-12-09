03551cafea270d150dc1d6c1c05676f8c927ff5a
Varsha Singh <varsha.singh0497@gmail.com>
2021-12-09 21:06:01 +0530

    fix: change `for` to `htmlFor`

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form id="form">
         <div className="form-control">
-          <label for="text">Text</label>
+          <label htmlFor="text">Text</label>
           <input type="text" id="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label for="amount">
+          <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
