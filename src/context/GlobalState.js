6ba9d115cc5eafddfd7e54c2c83d45fd044c7518
Varsha Singh <varsha.singh0497@gmail.com>
2021-12-09 21:26:31 +0530

    feat: create context for global state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,6 @@
-import React, { createContext, userReducer0 } from "react";
+import React, { createContext, useReducer } from "react";
+import AppReducer from './AppReducer';
+
 
 // initial state
 const initialState = {
@@ -9,3 +11,17 @@ const initialState = {
     { id: 4, text: "Camera", amount: 150 },
   ],
 };
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>)
+}
